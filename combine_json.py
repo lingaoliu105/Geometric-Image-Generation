e91e7f2f8d23f088da0b3044d4d0fd8351d84e9e
lingao <lingaoliu105@gmail.com>
2024-09-16 20:55:18 +0800

    visualizing segmentation

--- combine_json.py
+++ combine_json.py
@@ -5,15 +5,18 @@ from PIL import Image
 generate_num = 10
 # if len(sys.argv) > 1 and sys.argv[1]:
 #     generate_num = int(sys.argv[1])
+get_image_id = (x for x in range (1000000)).__next__
+get_annotation_id = (x for x in range (100000)).__next__
 
 labels_dict = {"info":{},"licenses":[],"categories":[],"images":[],"annotations":[]}
 for i in range (generate_num):
-    license = {"id":i}
+    image_id = get_image_id()
+    license = {"id":image_id}
     with Image.open(f"./output_png/new{i}.png") as img:
         width, height = img.size
     image = {
-        "id": i,
-        "license": i,
+        "id": image_id,
+        "license": license["id"],
         "file_name": f"new{i}.png",
         "height": height,
         "width": width,
@@ -21,30 +24,38 @@ for i in range (generate_num):
     }
     with open(f"./output_json/new{i}.json", "r") as file:
         data = json.load(file)
-    # print(data)
-    vertices = []
     for panel in data:
         for shape in panel["shapes"]:
+            vertices = []
+            segmentation = []
             coordinates = shape["base_geometry"]["coordinates"][0]
             for coordinate in coordinates:  
-                vertices.append(coordinate[0]/20*width+width/2)
-                vertices.append(height/2 - coordinate[1] / 20 * height)
+                transformed_coord = [coordinate[0]/20*width+width/2,height/2 - coordinate[1] / 20 * height]
+                vertices += transformed_coord              
                 vertices.append(2)
-    annotation = {
-        "id": i,
-        "image_id": i,
-        # "category_id": 2,
-        # "bbox": [260, 177, 231, 199],
-        # "segmentation": [...],
-        "keypoints": vertices,
-        "num_keypoints": len(vertices)/3,
-        # "score": 0.95,
-        # "area": 45969,
-        # "iscrowd": 0,
-    }
+                segmentation+= transformed_coord
+            segmentation_x_coords = [segmentation[x] for x in range(0,len(segmentation),2)]
+            segmentation_y_coords = [segmentation[x] for x in range(1,len(segmentation),2)]
+            shape_annotation = {
+                "id": get_annotation_id(),
+                "image_id": image_id,
+                # "category_id": 2,
+                "bbox": [min(segmentation_x_coords),min(segmentation_y_coords),max(segmentation_x_coords)-min(segmentation_x_coords),max(segmentation_y_coords)-min(segmentation_y_coords)],
+                "segmentation": [segmentation],
+                "keypoints": vertices,
+                "num_keypoints": len(vertices)/3,
+                # "score": 0.95,
+                # "area": 45969,
+                "iscrowd": 0,
+            }
+            labels_dict["annotations"].append(shape_annotation)
+    
+    # information unique to an image
     labels_dict["licenses"].append(license)
-    labels_dict["annotations"].append(annotation)
     labels_dict["images"].append(image)
+    
+    
+    labels_dict["annotations"].append(shape_annotation)
 
 with open("./my_dataset/labels.json","w") as json_file:
     json.dump(labels_dict,json_file,indent=4)
---
+++ dataset_visualization.py
@@ -0,0 +1,28 @@
+import fiftyone as fo
+import fiftyone.utils.data as foud
+import fiftyone.types as fot
+import fiftyone.zoo as foz
+
+
+# Path to your dataset (images or videos)
+dataset_dir = "./my_dataset"
+
+# Create a dataset from COCO format
+dataset = fo.Dataset.from_dir(
+    dataset_type=fot.COCODetectionDataset,
+    dataset_dir=dataset_dir,
+    # label_types=["detections", "segmentations"],
+)
+# dataset = foz.load_zoo_dataset(
+#     "coco-2017",
+#     split="validation",
+#     dataset_name="coco-2017-validation",
+#     label_types=["detections","segmentations"],  # Load only detection labels
+# )
+
+# while True:
+#     pass
+
+# Start the FiftyOne app to visualize the dataset
+session = fo.launch_app(dataset)
+session.wait()
