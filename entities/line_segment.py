fix wrong shape-line intersection & visualizing chain

entities/line_segment.py
@@ -4,14 +4,20 @@ import sys
 from networkx import center
 import numpy as np
 from shapely import LineString, Point
-import common_types
+from common_types import *
 from entities.entity import Entity, OpenShape, VisibleShape
 from typing import Literal, Optional, Union
 
 import generation_config
 import img_params
 from tikz_converters import LineSegmentConverter
-from util import get_line_rotation, get_point_distance, get_rand_point, rotate_point
+from util import (
+    almost_equal,
+    get_line_rotation,
+    get_point_distance,
+    get_rand_point,
+    rotate_point,
+)
 from shapely.affinity import translate
 from shapely.ops import nearest_points
 
@@ -116,7 +122,7 @@ class LineSegment(OpenShape):
     def shift(self, offset: np.ndarray):
         self.base_geometry = translate(self.base_geometry, offset[0], offset[1])
 
-    def set_endpoints(self, pt1: common_types.Coordinate, pt2: common_types.Coordinate):
+    def set_endpoints(self, pt1: Coordinate, pt2: Coordinate):
         self.base_geometry = LineString([pt1, pt2])
 
     def find_fraction_point(self, fraction: float):
@@ -137,13 +143,29 @@ class LineSegment(OpenShape):
         return self.base_geometry.intersects(other.base_geometry)
 
     def expand_fixed(self, length):
-        self.base_geometry.buffer(length)
+        if almost_equal(length,0.0):
+            pass
+        elif length > 0:
+            self.base_geometry = self.base_geometry.buffer(length)
+        elif length < 0:
+            self.scale(1 - 2 * abs(length) / self.length)
         return self
 
     def scale(self, ratio):
-        offset = self.endpt_left - self.center
+        """scale the line segment with the center as pivot
+
+        Args:
+            ratio (_type_): _description_
+        """
+        self.scale_with_pivot(ratio, self.center)
+
+    def scale_with_pivot(self, ratio, pivot: Coordinate):
+        assert self.base_geometry.contains(Point(pivot))
+        pivot = np.array(pivot)
+        offset1 = self.endpt_left - pivot
+        offset2 = self.endpt_right - pivot
         self.base_geometry = LineString(
-            self.center + offset * ratio, self.center - offset.ratio
+            self.center + offset1 * ratio, self.center + offset2.ratio
         )
 
     def expand(self, ratio):
@@ -165,15 +187,15 @@ class LineSegment(OpenShape):
             if maximum_achievable_distance < interval:
                 print("interval too large", file=sys.stderr)
                 return
+            nearest_point_on_shape, _ = nearest_points(
+                other_copy.base_geometry, mid_point
+            )
+            perpendicular_line_rotation = get_line_rotation(
+                nearest_point_on_shape.coords[0], self.center
+            )
             if (
                 minimum_achievable_distance <= interval
             ):  # the interval is achievable with rotation
-                nearest_point_on_shape, _ = nearest_points(
-                    other_copy.base_geometry, mid_point
-                )
-                perpendicular_line_rotation = get_line_rotation(
-                    nearest_point_on_shape.coords[0], self.center
-                )
                 upper, lower = perpendicular_line_rotation, self.rotation
                 while (
                     abs(upper - lower)
@@ -186,5 +208,17 @@ class LineSegment(OpenShape):
                         lower = mid
                     else:
                         upper = mid
+            else:
+                self.rotate(self.center, perpendicular_line_rotation - self.rotation)
+
         else:
             return
+
+    # def adjust_by_scaling(self,other:VisibleShape,interval:float,pivot:Coordinate):
+    #     # if almost_equal(pivot,self.endpt_up) or almost_equal(pivot,self.endpt_down):
+    #     # check if the desired interval is achievable by simply scaling
+    #     cpy = self.copy
+    #     cpy.scale_with_pivot((generation_config.GenerationConfig.canvas_height + generation_config.GenerationConfig.canvas_width)*10/cpy.length)
+    #     minimum_achievable_distance = cpy.base_geometry.distance(other.base_geometry)
+    #     if minimum_achievable_distance <= interval:
+    #         min_ratio =

entities/simple_shape.py
@@ -1,5 +1,6 @@
 import copy
 from typing import Optional
+from entities.line_segment import LineSegment
 import generation_config
 from img_params import *
 import math
@@ -13,7 +14,7 @@ import json
 from tikz_converters import SimpleShapeConverter
 import uid_service
 from entities.entity import ClosedShape, Entity, VisibleShape
-from util import choose_param_with_beta
+from util import almost_equal, choose_param_with_beta
 from generation_config import GenerationConfig
 
 
@@ -210,7 +211,7 @@ class SimpleShape(ClosedShape):
         self.size = new_size
         self.compute_base_geometry()
 
-    def search_touching_size(self, other: "SimpleShape"):
+    def search_touching_size(self, other: VisibleShape):
         """with a initial size that guarantees to overlap, search the appropriate size that touches the other shape (with tolerance defined in the class), and set the own size to it
 
         Args:
@@ -228,7 +229,7 @@ class SimpleShape(ClosedShape):
             self.set_size(mid)
             if self.base_geometry.overlaps(
                 other_shape
-            ) or self.base_geometry.intersects(other_shape):
+            ) or self.base_geometry.intersects(other_shape) or self.base_geometry.contains(other_shape):
                 upper = mid
             else:
                 lower = mid
@@ -238,12 +239,13 @@ class SimpleShape(ClosedShape):
     def search_size_by_interval(self,other:"VisibleShape",interval:float):
         padded_other = other.copy.expand_fixed(interval)
         self.search_touching_size(padded_other)
-
+            
     def shift(self, offset: np.ndarray):
         self.position += offset
         self.compute_base_geometry()
 
     def expand_fixed(self, length):
+        assert self.size +length >= 0
         self.set_size(self.size + length)
         return self
 

image_generators/chaining_image_generator.py
@@ -1,5 +1,4 @@
-
-
+from math import ceil
 from common_types import *
 from entities.line_segment import LineSegment
 from entities.simple_shape import SimpleShape
@@ -11,22 +10,12 @@ from util import *
 class ChainingImageGenerator(ImageGenerator):
     def __init__(self) -> None:
         self.position:Coordinate = (0,0)
-        self.draw_chain = False
+        self.draw_chain = True
         self.chain_shape = "line"
         self.shapes_list = []
-        self.element_num = 1
-        self.interval = 0.2
- 
-        
-        
-    def get_chain(self,curve_function):
-        curve_point_set = curve_function()
-        step_length = len(curve_point_set) // self.element_num
-        chain = [
-            curve_point_set[step_length * index] for index in range(self.element_num)
-        ]  # the set of all centers of the element shapes
-        return chain
-        
+        self.element_num = ceil(generate_beta_random_with_mode(0.4, 2) * 19) + 1
+        self.interval = 0.0
+
     def generate(self) -> Panel:
         """generate a composite geometry entity by chaining simple shapes
 
@@ -37,16 +26,25 @@ class ChainingImageGenerator(ImageGenerator):
 
         assert self.element_num >= 2 and self.element_num <= 20
         # composite the image first, then shift to the center pos
-
         if self.chain_shape == "bezier":
-            chain = self.get_chain(lambda: generate_bezier_curve_single_param(1))
+            curve_function = lambda: generate_bezier_curve_single_param(1)
         elif self.chain_shape == "circle":
-            chain = self.get_chain(lambda: generate_circle_curve(random.randrange(4, 8)))
+            curve_function = lambda: generate_circle_curve(random.randrange(4, 8))
         elif self.chain_shape =="line":
-            chain = self.get_chain(lambda:get_points_on_line((-5.0,5.0),(5.0,-5.0)))
+            curve_function = lambda:get_points_on_line((-5.0,5.0),(5.0,-5.0))
         else:
             print("curve type not assigned")
             raise
+        curve_point_set = curve_function()
+        def get_chain():
+            step_length = len(curve_point_set) // self.element_num
+            chain = [
+                curve_point_set[step_length * index] for index in range(self.element_num)
+            ]  # the set of all centers of the element shapes
+            return chain
+
+        chain = get_chain()
+        chain_shapes = [LineSegment(curve_point_set[i],curve_point_set[i+1],img_params.Color.black) for i in range(len(curve_point_set)-2)]
 
         shapes = []  # stack of shapes
 
@@ -69,7 +67,7 @@ class ChainingImageGenerator(ImageGenerator):
                         )
                     else:
                         element_size = get_point_distance(chain[i], shapes[-1].position) * 2
-                        
+
                     a = random.random()
                     if a<0.6:
                         element = SimpleShape(
@@ -81,11 +79,11 @@ class ChainingImageGenerator(ImageGenerator):
                         if i != 0:
                             element.search_size_by_interval(shapes[-1],self.interval)
                     else:
-                        offset = np.array([3,0])
+                        offset = np.array([1,0])
                         element = LineSegment(chain[i] + offset,chain[i] - offset)
                         if i!=0:
                             element.adjust_by_interval(shapes[-1],self.interval,prior_method="rotate")
-                        
+
                     shapes.append(element)
                 flag = False
             except AssertionError:
@@ -99,6 +97,6 @@ class ChainingImageGenerator(ImageGenerator):
         return Panel(
             top_left=self.panel_top_left,
             bottom_right=self.panel_bottom_right,
-            shapes=shapes,
+            shapes=shapes+chain_shapes if self.draw_chain else shapes,
             joints=touching_points,
         )